700a7b2363ffa79f8fd7f44a01425c0ece939b9d
nickforce989 <nic.forz@gmail.com>
2023-05-12 15:14:46 +0100

    Generalisation of plaquette analysis files, and leaving .cc code untouched

--- analysis_code/parameter_scan/change_mass.sh
+++ analysis_code/parameter_scan/change_mass.sh
@@ -6,12 +6,12 @@
 ####################################################################
 
 if [[ $# -eq 0 ]] ; then
-    echo 'Please provide a value for beta as an argument'
+    echo 'Please provide a value for beta and Nf_2AS_flavours as an argument'
     exit 1
 fi
 
 # Replace this value of beta
-sed -i "s/beta = [^;]*;/beta = $1 ;/" ../../../tests/sp2n/Test_hmc_Sp_Wilson_4_2ASFermionGauge.cc
+sed -i "s/--beta [^\\ ]*/--beta $1/" test
 
 # Load Modules
 module load compiler/gnu/10/2.0
@@ -30,13 +30,13 @@ for ASmass in $(seq -f "%.1f" -1.4 0.1 0.0); do
 
     # Replace the last line of the file called 'test'
     beta_times_10=$(echo "$1*10" | bc | awk '{printf("%d", $1)}')
-    sed -i "\$s/.*/    --Thermalizations 1000000 > .\/Nf4_tests\/hmc_\${SLURM_JOB_ID}-b${beta_times_10}_am-$n.out/" test
+    sed -i "\$s/.*/    --Thermalizations 100 > .\/Nf4_tests\/hmc_\${SLURM_JOB_ID}-b${beta_times_10}_am-$n.out/" test
 
     # Replace the value of ASmass
-    sed -i "s/ASmass = [^;]*;/ASmass = $ASmass;/" ../../../tests/sp2n/Test_hmc_Sp_Wilson_4_2ASFermionGauge.cc
+    sed -i "s/--fermionmass [^\\ ]*/--fermionmass $ASmass/" test
 
     # Compile the code
-    make Test_hmc_Sp_Wilson_4_2ASFermionGauge
+    make Test_hmc_Sp_Wilson_$2_2ASFermionGauge
 
     # Launch the job
     sbatch test
--- analysis_code/parameter_scan/parameter_scan_submit.sh
+++ analysis_code/parameter_scan/parameter_scan_submit.sh
@@ -10,10 +10,13 @@
 # Define the values of beta to loop over
 betas=(6.6 6.7 6.8 6.9 7.0 7.1)
 
+# Number of flavours for 2AS fermions
+nf=4
+
 for beta in "${betas[@]}"
 do
   # Variate mass
-  bash change_mass.sh $beta
+  bash change_mass.sh $beta $nf
 
   # Wait for all jobs to complete, supposed to last 6 hours
   sleep 6h
