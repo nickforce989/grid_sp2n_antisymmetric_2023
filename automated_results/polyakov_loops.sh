63086919d5a46a6a4ed88c7a49ed7a4fc9ada606
nickforce989 <nic.forz@gmail.com>
2023-06-26 15:33:01 +0100

    Little fix

--- automated_results/polyakov_loops.sh
+++ automated_results/polyakov_loops.sh
@@ -2,16 +2,24 @@
 
 initial_tests_dir="../../precomputed_data/polyakov_loops/"
 
+# Function to check if a directory is empty except for hidden files
+is_dir_empty_except_hidden() {
+  local dir=$1
+  local files_count=$(find "$dir" -maxdepth 1 -type f ! -iname ".*" | wc -l)
+  [[ $files_count -eq 0 ]]
+}
+
+
 cd ../analysis_code/polyakov_loops
 
-if [ -z "$(find "$initial_tests_dir" -mindepth 1 -type f -not -path '*/\.*')" ]; then
+if is_dir_empty_except_hidden "$initial_tests_dir"; then
   python polyloops_analysis.py
 fi
 
 
 cd ../../plots/codes/
 
-if [ -z "$(find "$initial_tests_dir" -mindepth 1 -type f -not -path '*/\.*')" ]; then
+if is_dir_empty_except_hidden "$initial_tests_dir"; then
   python plot_poly.py
 else
   python plot_poly.py --dp
